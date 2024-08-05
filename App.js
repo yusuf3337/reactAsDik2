a6eaa5a851cedd386f4f5e3b6657ef5bc8fe1cab
Yusuf Çepni <myusufcepni2@gmail.com>
2024-08-05 22:26:12 +0300

    haciBuneAmk

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text,
+  View,
+  TextInput,
+  Pressable
+   } from 'react-native';
+
+import React, {useState} from 'react';
 
 export default function App() {
+
+const [name,setName] = useState("")
+const [password,setPassword] = useState("")
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Name</Text>
+      <TextInput
+       style={styles.textInputStyle}
+       placeholder='Enter Your Name'
+       onChangeText={setName}
+       />
+
+      <Text>Password</Text>
+      <TextInput
+      style={styles.textInputStyle}
+      keyboardType='numeric'
+      placeholder='Enter Your Password'
+      onChangeText={setPassword}
+      />
+
+      <Pressable
+        onPress={()=> console.log("clicked")}
+        style={styles.buttonStyle}>
+
+        <Text style={styles.buttonText}>Save</Text>
+
+      </Pressable>
     </View>
   );
 }
@@ -17,4 +49,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInputStyle:{
+    borderWidth:1,
+    height:45,
+    width:250,
+    marginVertical:8,
+    padding:10,
+    borderRadius:20,
+    textAlign:'center'
+  },
+  buttonStyle:{
+    width:80,
+    height:30,
+    borderRadius:20,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'orange',
+  },
+  buttonText:{
+    color:'white',
+    fontWeight:'bold'
+  }
 });
